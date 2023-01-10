391678375d99ce0d83f3e7c30c5ea12436cad1aa
Rajat Srivastava <rajat.sedate@gmail.com>
2023-01-10 16:54:25 +0530

    Added a commet for the fire and forget job

--- hangfire-demo/Jobs/Indexing/IndexingJobProvider.cs
+++ hangfire-demo/Jobs/Indexing/IndexingJobProvider.cs
@@ -20,6 +20,9 @@ namespace hangfire_demo.Jobs.Indexing
 			CyptoTrackerService tracker = new CyptoTrackerService("", rapidApiKey);
 			var currencies = await tracker.GetCurrencies();
 
+			//this is for demo purpose, if run multiple times same record will be inserted again and again. Since it is indexing
+			//maybe every time this is fired we can delete the records and insert a new one.
+			//or make sure it is executed once
 			_repository.BulkInsert(currencies);
 		}
 	}
